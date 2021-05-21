c7f2947a34be2ec4728225d85b7947ae6bb62d31
akselnf <akselfor@gmail.com>
2021-05-21 12:00:15 +0200

    test

---
+++ bio382_git.R
@@ -0,0 +1,16 @@
+## install if needed (do this exactly once):
+#install.packages("usethis")
+
+
+library(usethis)
+use_git_config(
+  user.name = "akselnf", 
+  user.email = "akselfor@gmail.com"
+)
+
+create_github_token()
+gitcreds::gitcreds_set()
+
+use_git()
+
+#kjhkjh
